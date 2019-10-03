c4f1dfa708f321e125c6d451bcd85b4f6022b08e
shackie.p <phuongshaquile@gmail.com>
2019-10-03 13:26:32 -0700

    adding sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    if (num1 > num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    let newNum = 0;
+    for (let i = num1; i < num2 + 1; i++) {
+        newNum += parseInt(i);
+    }
+    return newNum;
 }
 
 module.exports = sumAll

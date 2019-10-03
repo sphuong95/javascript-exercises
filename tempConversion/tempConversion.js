ccca19c7ff3b71f29cf51f5ab2a95c42a64dff21
shackie.p <phuongshaquile@gmail.com>
2019-10-03 14:39:50 -0700

    adding tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * 5 / 9;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (temp * 9 / 5) + 32;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 }
 
 module.exports = {

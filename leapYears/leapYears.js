010da314959247bbdb87b46b0460a9dc1146e61c
shackie.p <phuongshaquile@gmail.com>
2019-10-03 14:25:16 -0700

    adding leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //if they are divisible by 4
+    if (year % 4 == 0 ){
+        //non century years
+        if (year % 100 !== 0) {
+                return true;
+            }
+        //years divisble by 100
+        else if ((year/100)%4 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 }
 
 module.exports = leapYears

8afd8c3319b35f17d36b9a6151b6770f395354df
shackie.p <phuongshaquile@gmail.com>
2019-10-15 22:37:02 -0700

    completed calculator portion

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,51 @@
-function add () {
-	
+function add (num1, num2) {
+	let sum = num1 + num2;
+	return sum;
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	let sum = num1 - num2;
+	return sum;
 }
 
-function sum () {
-	
+function sum (array1) {
+	let sum = 0;
+	if (array1.length == 0) {
+		sum = 0;
+	}
+	else {
+		for (let i = 0; i < array1.length; i++) {
+			sum += parseInt(array1[i]);
+		}
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (array1) {
+	let sum = array1[0];
+	if (array1.length == 0) {
+		sum = 0;
+	}
+	else {
+		for (let i = 1; i < array1.length; i++) {
+			sum *= array1[i];
+		}
+	}
+	return parseInt(sum);
 }
 
-function power() {
-	
+function power(num1, num2) {
+	let sum = num1 ** num2;
+	return sum;
 }
 
-function factorial() {
-	
+function factorial(num1) {
+	let sum = 1;
+	while (num1 !== 0) {
+		sum *= num1;
+		num1--;
+	}
+	return sum;
 }
 
 module.exports = {
